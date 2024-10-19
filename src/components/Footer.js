ca0afde83d690f66f005cc22b1deca0dda23363e
Shivam Sharma <shivam0sharma85@gmail.com>
2024-10-19 18:06:22 +0530

    make changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,7 +15,7 @@ function Footer() {
           <h3>Designed and Developed by Shivam Sharma</h3>
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3></h3>
+          <h3>WORK HARD, CODE HARDER</h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
